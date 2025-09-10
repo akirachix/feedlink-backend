a054854c4b7bba400bf131fbebc2efbb06dac507
kisanet shshay <shshaykisanet@gmail.com>
2025-09-10 12:10:23 +0300

    Created inventory endpoint

---
+++ api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from inventory.models import Listing
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = '__all__'
+        read_only_fields = ['listing_id', 'status', 'created_at', 'updated_at', 'producer']
+
+    def validate(self, data):
+    
+        if not data.get('image') and not data.get('image_url'):
+            raise serializers.ValidationError("Either image or image_url must be provided.")
+        return data
