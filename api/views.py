f6c729a5a2129da3cd47e5168cb0e73cec56d8f6
hewan <hewaanmehari@gmail.com>
2025-09-10 18:33:08 +0300

    updated views

--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,  mixins
 from rest_framework.response import Response
 from rest_framework import status
 from reviews.models import Review          
 from .serializers import ReviewSerializer  
 
-class ReviewViewSet(viewsets.ModelViewSet):
+class ReviewViewSet(
+    mixins.ListModelMixin,      
+    mixins.CreateModelMixin,    
+    viewsets.GenericViewSet
+):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
--- reviews/tests.py
+++ reviews/tests.py
@@ -7,3 +7,15 @@ class ReviewModelTest(TestCase):
         self.assertEqual(review.ratings, 5)
         self.assertIsNotNone(review.review_id)
         self.assertEqual(str(review), f"Review {review.review_id} - {review.ratings} stars")
+
+    def test_create_review_with_2_stars(self):
+        review = Review.objects.create(ratings=2)
+        self.assertEqual(review.ratings, 2)
+        self.assertIsNotNone(review.review_id)
+        self.assertEqual(str(review), f"Review {review.review_id} - {review.ratings} stars")
+
+    def test_create_review_with_1_star(self):
+        review = Review.objects.create(ratings=1)
+        self.assertEqual(review.ratings, 1)
+        self.assertIsNotNone(review.review_id)
+        self.assertEqual(str(review), f"Review {review.review_id} - {review.ratings} stars")
