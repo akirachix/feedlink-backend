338d83f1d457f9e37429c7a595e8dfa6eb12aaa4
emelineniyogisubizo <rubayizaniyogisubizoemeline@gmail.com>
2025-09-11 22:23:10 +0300

    refined tests

--- api/urls.py
+++ api/urls.py
@@ -21,15 +21,16 @@ router.register(r'wasteclaims', WasteClaimViewSet)
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'listings', ListingViewSet, basename='listings')
 
+
 urlpatterns = [
 
-    path('/', include(router.urls)),
     path('signup/', UserSignupAPIView.as_view(), name='user-signup'),
     path('login/', UserLoginAPIView.as_view(), name='user-login'),
     path('forgotpassword/', ForgotPasswordView.as_view(), name='forgot-password'),
     path('verification/', VerifyCodeView.as_view(), name='verify-code'),
     path('reset/', ResetPasswordView.as_view(), name='reset-password'),
-    path('api/listings/upload-csv/', ListingCSVUploadView.as_view(), name='listing-csv-upload'),
+    path('listings/upload-csv/', ListingCSVUploadView.as_view(), name='listing-csv-upload'),
+     path('', include(router.urls)),
 
 ]
 
