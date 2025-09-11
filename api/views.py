b8da532927889f12fc0a36cc93abeb2f14ac5d6d
hewan <hewaanmehari@gmail.com>
2025-09-11 09:40:26 +0300

    Created reviews endpoint

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets,  mixins
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from reviews.models import Review          
