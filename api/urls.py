95a7a4666c1e71c9e389a49efec860b19adc49e5
hewan <hewaanmehari@gmail.com>
2025-09-12 10:00:59 +0300

    Updated api urls

--- api/urls.py
+++ api/urls.py
@@ -6,14 +6,12 @@ from .views import ListingViewSet, ListingCSVUploadView
 
 router = DefaultRouter()
 router.register(r'reviews', ReviewViewSet, basename='review')
-
 router.register(r'listings', ListingViewSet, basename='listings')
 
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api/listings/upload-csv/', ListingCSVUploadView.as_view(), name='listing-csv-upload'),
-
 
 ]
 
