dfab1a83e19ab8e977b96c6d1291116c508982fb
emelineniyogisubizo <rubayizaniyogisubizoemeline@gmail.com>
2025-09-17 15:20:16 +0300

    fixed settings

--- feedlink/settings.py
+++ feedlink/settings.py
@@ -30,11 +30,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",   
-    "http://localhost:3002",   
-    "http://127.0.0.1:3002", 
-    "http://127.0.0.1:8000", 
-    "https://yourdomain.com",
+    "http://localhost:3000",       
+    "http://127.0.0.1:8000",   
 ]
 
 
