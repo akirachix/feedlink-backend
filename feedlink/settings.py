b1e4d13fc3dec7f68c91768bdce6a30892710413
hewan <hewaanmehari@gmail.com>
2025-09-19 13:10:45 +0300

    changed the database to be postgres

--- feedlink/settings.py
+++ feedlink/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-insecure-key-for-dev-only')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
