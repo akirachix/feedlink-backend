bcbfcd882443d84add20393ee1fbda4830e4e251
hewan <hewaanmehari@gmail.com>
2025-09-19 09:18:05 +0300

    updated the files

--- feedlink/settings.py
+++ feedlink/settings.py
@@ -122,14 +122,17 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-   'DEFAULT_AUTHENTICATION_CLASSES': [
-       'rest_framework_simplejwt.authentication.JWTAuthentication',
-   ],
-   'DEFAULT_PERMISSION_CLASSES': [
-       'rest_framework.permissions.AllowAny',
-   ],
-   'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+  
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+
+
+
+
 
 
 
