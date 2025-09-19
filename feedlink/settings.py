c13dc3fe86897ffa6d1621e9e302cea1a6621adf
hewan <hewaanmehari@gmail.com>
2025-09-19 11:34:27 +0300

    updated the files

--- feedlink/settings.py
+++ feedlink/settings.py
@@ -26,14 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-insecure-key-for-dev-only')
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-
 ALLOWED_HOSTS = ['*']
 
 
@@ -114,10 +110,14 @@ WSGI_APPLICATION = 'feedlink.wsgi.application'
 
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'feedlink',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',  
+        'PORT': '5432',       
+    }
 }
 
 
